74a5d41e40bbcb7be4ade1b60883731e6d54b03e
cuongvu <cuongvc93@gmail.com>
2015-01-28 10:42:57 +0700

    update

--- my_module/respon_object/job.js
+++ my_module/respon_object/job.js
@@ -2,10 +2,10 @@ module.exports					=	function(res, object){
 	object.save(function(err){
 		if (err){
 			console.log(err);
-			res.json({error_code : 1, job : null});
+			res.write(JSON.stringify({error_code : 1, job : null}));
 			res.status(200).end();
 		} else{
-			res.json({error_code : 0, job : object});
+			res.write(JSON.stringify({error_code : 0, job : object}));
 			res.status(200).end();			
 		}
 	})
