8bd0f695a013d167e4644af9f7e740f84ab618a5
Thanchet <phamvanhieu861992@gmail.com>
2015-01-31 11:34:55 +0700

    update

--- public/js/global.js
+++ public/js/global.js
@@ -1,5 +1,5 @@
-	// var BASE_URL            = 'http://job.dev';
-	var BASE_URL            = 'http://jobpitch.campcoders.com';
+	var BASE_URL            = 'http://job.dev';
+	// var BASE_URL            = 'http://jobpitch.campcoders.com';
 	var STR_UPLOAD_IMAGE    = BASE_URL + '/api/upload_photo';
 	var STR_API_CREATE_JOB  = BASE_URL + '/api/create_job';
 	var STR_API_GETP_RECENT = BASE_URL + '/api/recent';
