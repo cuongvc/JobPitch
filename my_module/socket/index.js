3897b5c3690ca1c122419c8b0061d58a18bddc5e
cuongvu <cuongvc93@gmail.com>
2015-02-12 17:13:50 +0700

    clear console.log

--- my_module/socket/index.js
+++ my_module/socket/index.js
@@ -10,10 +10,8 @@ var user_sockets = {};
 
 io_notify.use(function(socket, next){
 
-	console.log('\n HAVE A REQUEST \n');
   user_id = socket.request._query.user_id;
   token   = socket.request._query.token;
-  console.log('\n HAVE A REQUEST \n');
 
   next();
   // check_token(user_id, token, function(exist, user_exist){
@@ -31,10 +29,8 @@ io_notify.use(function(socket, next){
 
 
 io_notify.on('connection', function(socket){
-  console.log('connection');
 
 	socket.on('disconnect', function(){
-      console.log(socket.user_id + ' disconnect');
       socket.broadcast.emit('One user off', socket.user_id);
  	    delete user_sockets[socket.user_id];
       list_user.splice(list_user.indexOf(socket.user_id));
