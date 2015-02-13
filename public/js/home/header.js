8afb5ffe39d42957ced6d59bd577867f35b75bfd
Thanchet <phamvanhieu861992@gmail.com>
2015-02-13 16:48:31 +0700

    update

--- public/js/home/header.js
+++ public/js/home/header.js
@@ -6,6 +6,22 @@ Header.directive('header',function(){
 		link: function(scope,element,attrs){
 			$('body').click(function(){
 				$('.notification-box').addClass('hidden');
+				
+			})
+			$('.notification-box').bind('mousewheel DOMMouseScroll', function(e) {
+			    var scrollTo = null;
+
+			    if (e.type == 'mousewheel') {
+			        scrollTo = (e.originalEvent.wheelDelta * -1);
+			    }
+			    else if (e.type == 'DOMMouseScroll') {
+			        scrollTo = 40 * e.originalEvent.detail;
+			    }
+
+			    if (scrollTo) {
+			        e.preventDefault();
+			        $(this).scrollTop(scrollTo + $(this).scrollTop());
+			    }
 			})
 		},
 	}
