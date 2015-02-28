532873a5bf76cacfb089e5b29a1dde6e7431a1d9
Thanchet <phamvanhieu861992@gmail.com>
2015-02-27 16:51:07 -0800

    update search

--- public/js/home/search-result.js
+++ public/js/home/search-result.js
@@ -15,10 +15,10 @@ TemplateApp.controller('SearchResultCtrl',function($scope,$http,$routeParams,SEA
 	SearchService.then(function(response){
 		console.log(response);
 		if(response.error_code == 0){
-			SearchResultJobs = JOB.JobHandler(response.results.jobs.results,$scope.user._id);
+			SearchResultJobs = JOB.JobHandler(response.jobs,$scope.user._id);
 			$scope.SearchResultJobs = SearchResultJobs;
 
-			SearchResultPitch = PITCH.getPitchSidebarHandler(response.results.applications.results,$scope.user._id);
+			SearchResultPitch = PITCH.getPitchSidebarHandler(response.applications,$scope.user._id);
 			$scope.SearchResultPitch = SearchResultPitch;
 			if(SearchResultJobs.length == 0 && SearchResultPitch.length == 0){
 				alert('No result found');
